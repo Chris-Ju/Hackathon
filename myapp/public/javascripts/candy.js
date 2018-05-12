ef0998bfd4df2db564ce5cdfa0777f59babeeb18
zhulinyin <lijt26@mail2.sysu.edu.cn>
2018-05-13 06:54:09 +0800

    update

--- myapp/public/javascripts/candy.js
+++ myapp/public/javascripts/candy.js
@@ -9,13 +9,36 @@ $(document).ready(function () {
           var content = obj.content;
           var date = obj.date;
           var commentNumber = obj.commentNumber;
-          var li = $("<li style='float:left;width:300px;'><div class='mr_zhe'><div class='mr_zhe_i' style='display:block'><p class='story'>" + content +
+          var random = obj.random;
+          
+          var li = $("<li id='" + random +
+            "' style='float:left;width:300px;'><div class='mr_zhe'><img src='images/star_y.png' class='star animated'/><div class='mr_zhe_i' style='display:block'><p class='story'>" + content +
             "</p><div class='mr_zhe_p'><h3><span>" + date +
             "</span>评论数（" + commentNumber +
             "）</h3></div></div><div class='mr_zhe_hover' style='top:190px;display:none'><h1/><div class='mr_zhe_p'><h3><span>" + date +
             "</span>评论数（" + commentNumber +
-            "）</h3></div></div></div></li>")
+            "）</h3></div></div></div></li>");
           $("#mr_fu").append(li);
+          li.bind('click', function () {
+            var random = $(this).attr('id');
+            $.ajax({
+              url: '/candy/comment',
+              type: 'POST',
+              dataType: 'jsonp',
+              data: {
+                random: random
+              },
+              statusCode: {
+                200: function (data) {
+                  var arr = eval(data.responseText);
+                  for (var i = 0; i < arr.length; i++) {
+                    var obj = arr[i];
+                    console.log(obj);
+                  }
+                }
+              }
+            });
+          });
         }
         /* 图片滚动效果 */
         $(".mr_frbox").slide({
@@ -31,24 +54,26 @@ $(document).ready(function () {
         $(".mr_zhe_hover").css("top", $('.mr_zhe').eq(0).height());
         $("li").mouseout(function (e) {
           if ((e.pageX < $(this).offset().left || e.pageX > ($(this).offset().left + $(this).width())) || (e.pageY < $(this).offset().top || e.pageY > ($(this).offset().top + $(this).height()))) {
+            $(this).children('.mr_zhe').children('.star').removeClass('zoomIn').hide(); 
             $(this).find('.mr_zhe_i').show();
             $(this).find('.mr_zhe_hover').hide().stop().animate({
               top: '190px'
             }, {
-                queue: false,
-                duration: 190
-              });
+              queue: false,
+              duration: 190
+            });
             return false;
           }
         });
         $('.mr_zhe').mouseover(function (event) {
+          $(this).children('.star').show().addClass('zoomIn');
           $(this).find('.mr_zhe_i').hide();
           $(this).find('.mr_zhe_hover').show().stop().animate({
             top: '190px'
           }, {
-              queue: false,
-              duration: 190
-            });
+            queue: false,
+            duration: 190
+          });
           return false;
         });
       }
