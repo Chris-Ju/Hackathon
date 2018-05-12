5a556c378622177f9d85c5354488db3f02b60933
zhulinyin <lijt26@mail2.sysu.edu.cn>
2018-05-12 16:27:42 +0800

    update

--- myapp/public/javascripts/user.js
+++ myapp/public/javascripts/user.js
@@ -6,8 +6,27 @@ function moveOut() {
   $("#left").removeClass("zoomIn").addClass("fadeOutLeft");
   $("#right").removeClass("zoomIn").addClass("fadeOutRight");
   setTimeout(function () {
-    $(".main #left").remove();
-    $(".main #right").remove();
+    $(".main #left").removeClass("fadeOutLeft").addClass("zoomIn");
+    $(".main #right").removeClass("fadeOutRight").addClass("zoomIn");
     //$("<img/>").attr("src", imgs[counter]).addClass("animated zoomIn").insertBefore($(".center p"));
-  }, 500);
+  }, 1000);
+}
+function submit() {
+  $.ajax({
+    url: '/regist',
+    type: 'POST',
+    dataType: 'jsonp',
+    data: {
+      username: username,
+      password: password,
+      phone: phone,
+      email: email
+    },
+    statusCode: {
+      200: function () {
+        deleteWarning();
+        window.location.href = "/";
+      }
+    }
+  });
 }
