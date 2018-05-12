c470eb27d1f96d7b811bcfa8ce8d6401f0365282
ChrisJu <592931812@qq.com>
2018-05-12 22:23:41 +0800

    FUCK

--- myapp/model/dataHandle.js
+++ myapp/model/dataHandle.js
@@ -59,16 +59,16 @@ function findOneStory(callback) {
     MongoClient.connect(url, function (err, client) {
         var dbase = client.db('mytestingdb');
         var random = Math.random();
-        dbase.collection('story').findOne({"random":{"$lt":random}}).toArray(function (err, result) {
+        dbase.collection('story').find({"random":{"$lt":random}}).toArray(function (err, result) {
             if (result.length == 0) {
                 dbase.collection('story').findOne({"random":{"$gte":random}}).toArray(function (err, result) {
                     client.close();
-                    callback(result);
+                    callback(result[0]);
                 });
                 
             } else {
                 client.close();
-                callback(result);            
+                callback(result[0]);            
             }
         });
     });
@@ -93,16 +93,16 @@ function findOneCandy(callback) {
     MongoClient.connect(url, function (err, client) {
         var dbase = client.db('mytestingdb');
         var random = Math.random();
-        dbase.collection('candy').findOne({"random":{"$lt":random}}).toArray(function (err, result) {
+        dbase.collection('candy').find({"random":{"$lt":random}}).toArray(function (err, result) {
             if (result.length == 0) {
                 dbase.collection('candy').findOne({"random":{"$gte":random}}).toArray(function (err, result) {
                     client.close();
-                    callback(result);
+                    callback(result[0]);
                 });
                 
             } else {
                 client.close();
-                callback(result);
+                callback(result[0]);
             }
         });
     });
