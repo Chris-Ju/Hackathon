62e8a91311752458d2356b64ec5af424ca1530d3
zhulinyin <lijt26@mail2.sysu.edu.cn>
2018-05-12 19:10:23 +0800

    update

--- myapp/public/javascripts/user.js
+++ myapp/public/javascripts/user.js
@@ -1,32 +1,60 @@
-$(function(){
-  $("#left").click(moveOut).hover(showIntroduction,hideIntroduction);
+var storyHouse = false;
+var sweetHouse = false;
+var story = false;
+var sweet = false;
+$(function () {
+  $("#left").click(moveOut).hover(showIntroduction, hideIntroduction);
   $("#right").click(moveOut).hover(showIntroduction, hideIntroduction);
 });
+
 function showIntroduction() {
   $(this).next('.intro').css('opacity', 1);
 }
+
 function hideIntroduction() {
   $(this).next('.intro').css('opacity', 0);
 }
+
 function moveOut() {
+
   $("#left").removeClass("zoomIn").addClass("fadeOutLeft");
   $("#right").removeClass("zoomIn").addClass("fadeOutRight");
-  setTimeout(function () {
-    $(".main #left").removeClass("fadeOutLeft").addClass("zoomIn");
-    $(".main #right").removeClass("fadeOutRight").addClass("zoomIn");
-    //$("<img/>").attr("src", imgs[counter]).addClass("animated zoomIn").insertBefore($(".center p"));
-  }, 1000);
+  if ($(this).attr("id") == "left") {
+    storyHouse = true;
+    setTimeout(function () {
+      $(".main #left").children(".title1").html('故事');
+      $(".main #left").children(".star").attr("src", "images/book.png");
+      $(".main #right").children(".title1").html('酒');
+      $(".main #right").children(".star").attr("src", "images/wine.png");
+      $(".main #left").removeClass("fadeOutLeft").addClass("zoomIn");
+      $(".main #right").removeClass("fadeOutRight").addClass("zoomIn");
+      //$("<img/>").attr("src", imgs[counter]).addClass("animated zoomIn").insertBefore($(".center p"));
+    }, 1000);
+  }
+  if ($(this).attr("id") == "right") {
+    sweetHouse = true;
+    setTimeout(function () {
+      $(".main #left").children(".title1").html("糖果");
+      $(".main #left").children(".star").attr("src", "images/book.png");
+      $(".main #right").children(".title1").html("贩卖机");
+      $(".main #right").children(".star").attr("src", "images/wine.png");
+      $(".main #left").removeClass("fadeOutLeft").addClass("zoomIn");
+      $(".main #right").removeClass("fadeOutRight").addClass("zoomIn");
+      //$("<img/>").attr("src", imgs[counter]).addClass("animated zoomIn").insertBefore($(".center p"));
+    }, 1000);
+  }
 }
+
 function submit() {
-  var niming=$("#niming").val();
-  var content=$("#content").val();
+  var niming = $("#niming").val();
+  var content = $("#content").val();
   $.ajax({
     url: '/user',
     type: 'POST',
     dataType: 'jsonp',
     data: {
       niming: niming,
-      content: content    
+      content: content
     },
     statusCode: {
       200: function () {
